a0737c3cecc71fccb8b324db28fba2c6bb951a85
GALL965 <bonaisdecamote45@gmail.com>
2025-05-04 13:08:02 -0600

    b

--- server/index.js
+++ server/index.js
@@ -1,16 +1,3 @@
-const { sequelize } = require('../database/models');
-const express = require('express');
-const cors = require('cors');
-const routes = require('../routes');
-
-const app = express();
-const PORT = 8080;
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api', routes); // rutas disponibles desde /api
-
 app.listen(PORT, async () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 
@@ -23,5 +10,6 @@ app.listen(PORT, async () => {
 
   } catch (err) {
     console.error('❌ Error conectando a la base de datos:', err.message);
+    console.error(err); // 👈 esto muestra todo el detalle en los logs de Render
   }
 });
